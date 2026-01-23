716cfb4635e24f40fdec329572a9cbe335e892a6
Sumanth <89512217+sumanth-appu@users.noreply.github.com>
2026-01-23 14:02:43 +0530

    insta icon removed

--- src/ConnectDetails.js
+++ src/ConnectDetails.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons';
 import './ContactDetails.css';
 
 const ContactDetails = () => {
